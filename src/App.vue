5101b26fecc2d5a048dd5d1cbfcf410644736cba
ulwl <ulwl@github.com>
2023-05-06 00:07:25 +0800

    1️⃣initialing the repository

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import MyHeader from './components/MyHeader/index.vue'
+import MyMain from './components/MyMain/index.vue'
+</script>
+<template>
+  <div class="container">
+    <header>
+      <MyHeader></MyHeader>
+    </header>
+    <main><MyMain></MyMain>
+    </main>
+    <footer></footer>
+  </div>
+</template>
+<style scoped lang="less">
+.container {
+  header {
+    height: 60px;
+  }
+}
+</style>
